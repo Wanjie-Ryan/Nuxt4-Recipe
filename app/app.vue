b2a91a1761657564b1dfdfa31f98d43836c1da48
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-11-15 09:30:55 +0300

    recipe app

---
+++ app/app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <!-- <NuxtRouteAnnouncer /> -->
+    <!-- <NuxtWelcome /> -->
+     <h1 class="text-lg text-cyan-600">Hey Tailwind</h1>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,41 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-04-03",
+
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  // the experimental allows you to toggle features that are still under testing or aren't finalized for stable use.
+  experimental: {
+    sharedPrerenderData: false,
+    compileTemplate: false,
+    resetAsyncDataToUndefined: false,
+    templateUtils: true,
+    relativeWatchPaths: true,
+    defaults: {
+      useAsyncData: {
+        deep: true,
+      },
+    },
+  },
+
+  unhead: {
+    renderSSRHeadOptions: {
+      omitLineBreaks: false,
+    },
+  },
+
+  devtools: { enabled: true },
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/google-fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+  ],
+  googleFonts: {
+    families: {
+      Montserrat: true,
+    },
+  },
+});
