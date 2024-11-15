8dd7ff091c55bb6fbacdbfa273c28f17a292533f
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-11-15 20:54:47 +0300

    nuxt dynamic routing and fetching data

--- app/pages/recipes/[id].vue
+++ app/pages/recipes/[id].vue
@@ -1,16 +1,23 @@
 <template>
-
-  
+  <h1>
+    {{ data.name }}
+  </h1>
+  <p>
+    {{ data.rating }}
+  </p>
 </template>
 
 <script setup>
+const { id } = useRoute().params;
 
-const {id} = useRoute().params;
+const { data, error } = await useFetch(`https://dummyjson.com/recipes/${id}`);
 
-const {data, error} = await useFetch(`https://dummyjson.com/recipes/${id}`);
-
+if (error.value) {
+  throw createError({
+    statusCode: error.value?.statusCode,
+    message: error.value?.statusMessage,
+  });
+}
 </script>
 
-<style>
-
-</style>
+<style></style>
